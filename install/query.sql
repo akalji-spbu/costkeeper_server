b7c100950cd97ef0e7b20eedb49dfcb7c963a09a
Nikolay Tikhonov <akalji@akaljis-laptop.local>
2016-10-14 19:35:28 +0300

    query modified

--- install/query.sql
+++ install/query.sql
@@ -1,41 +1,41 @@
 USE costkeeper_main;
 
-CREATE TABLE user(
-	id INT UNIQUE,
-  shop_name VARCHAR(20)
-);
-
-CREATE TABLE shops(
-	id INT UNIQUE,
-  shop_name VARCHAR(20)
+CREATE TABLE countries(
+	id INT UNSIGNED UNIQUE PRIMARY KEY,
+	name VARCHAR(20)
 );
 
 CREATE TABLE regions(
-	id INT UNIQUE,
-  region_name VARCHAR(20)
+	id INT UNSIGNED UNIQUE PRIMARY KEY,
+	name VARCHAR(20),
+	country_id VARCHAR(20)
 );
 
 CREATE TABLE cities(
-	id INT UNIQUE,
-  city_name VARCHAR(20),
-  zip_code MEDIUMINT UNSIGNED,
-  region_id INT
+	id INT UNSIGNED UNIQUE PRIMARY KEY,
+	name VARCHAR(20),
+	region_id VARCHAR(20)
 );
 
-CREATE TABLE goods(
-	id INT UNSIGNED UNIQUE,
-	barcode INT UNSIGNED
+CREATE TABLE streets(
+	id INT UNSIGNED UNIQUE PRIMARY KEY,
+	name VARCHAR(20)
 );
 
-CREATE TABLE baskets(
-	id INT UNIQUE,
-	user INT
+CREATE TABLE roles(
+	id INT UNSIGNED UNIQUE PRIMARY KEY,
+	name VARCHAR(20),
+	admin BOOLEAN
 );
 
-CREATE TABLE basket(
-	id INT UNIQUE
+CREATE TABLE users(
+	id INT UNSIGNED UNIQUE PRIMARY KEY,
+  nickname VARCHAR(20),
+	email VARCHAR(20),
+	firstname VARCHAR(20),
+	lastname VARCHAR(20),
+	role_id VARCHAR(20),
+	avatar VARCHAR(20),
+	password VARCHAR(20)
 );
 
-CREATE TABLE costs(
-	id INT UNIQUE
-);
