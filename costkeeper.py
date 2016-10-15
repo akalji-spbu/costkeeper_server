28bf882de09baef7f88f94a7fe0fb543edda3ec8
akalji <akalji@ya.ru>
2016-10-15 18:30:46 +0300

    Ыщьу сщььше

--- costkeeper.py
+++ costkeeper.py
@@ -2,6 +2,10 @@ import sys
 import string
 import os
 import config
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, create_engine
+
+Base = declarative_base()
 
 def clrscr():
     try:
@@ -9,3 +13,6 @@ def clrscr():
     except:
         os.system('cls')
 
+class Bountry(Base):
+    __tablename__ = 'countries'
+    Сountry_ID = Column(Integer, primary_key=True)
