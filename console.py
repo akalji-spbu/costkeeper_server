5a64970164716f731ffa4df59fe95571b3ee7d92
akalji <akalji@ya.ru>
2016-10-15 18:07:20 +0300

    some commit

--- console.py
+++ console.py
@@ -1,6 +1,5 @@
-import sqlalchemy
 import config
+import costkeeper
 
-db_uri = config.db_dialect + "://"+ config.db_user + ':' + config.db_password + '@' + config.db_host +":"+ config.db_port + '/'+ config.db_name
-print(db_uri)
+costkeeper.clrscr()
 
--- costkeeper.py
+++ costkeeper.py
@@ -1,39 +1,11 @@
 import sys
 import string
-from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import mapper, relationship
-from sqlalchemy.ext.declarative import declarative_base
-metadata = MetaData()
+import os
+import config
 
-class Good():
-    def __init__(self):
-        print("init done")
+def clrscr():
+    try:
+        os.system('clear')
+    except:
+        os.system('cls')
 
-class Basket():
-    def __init__(self):
-        print("init done")
-
-class User():
-    def __init__(self):
-        print("init done")
-
-class Shop():
-    def __init__(self):
-        print("init done")
-
-
-class City():
-    def __init__(self):
-        print("init done")
-
-class Region():
-    def __init__(self):
-        print("init done")
-
-class Baskets():
-    def __init__(self):
-        print("init done")
-
-class Costs():
-    def __init__(self):
-        print("init done")
