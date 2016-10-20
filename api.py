2a2f5f3246538a340b683badbf1965eb6e0e749c
akalji <akalji@ya.ru>
2016-10-20 18:05:15 +0300

    Added AUTH Class Added Handlers

--- api.py
+++ api.py
@@ -1,1 +1,35 @@
 from sqlalchemy import TEXT, INTEGER, String
+
+def UsersMethods(method,req):
+    if (method=="auth"):
+        token=user_auth()
+
+
+def ShopsMethods(method,req):
+    return("Hello World")
+
+def GoodsMethods(method,req):
+    return("Hello World")
+
+def CostsMethods(method,req):
+    return("Hello World")
+
+def BasketsMethods(method,req):
+    return("Hello World")
+
+
+
+#users methods
+def user_auth():
+    return("Hello World")
+
+
+def user_alter():
+    return("Hello World")
+
+
+def user_delete():
+    return("Hello World")
+
+
+#end users methods
--- service.py
+++ service.py
@@ -1,26 +1,69 @@
 import api
 import tornado.web
 import tornado.httpserver
+import json
 
 class PageOneHandler(tornado.web.RequestHandler):
     def get(self):
-        vari = self.get_argument('vari', True)
-        if(vari=="1"):
-            self.write("hello  world")
-        if(vari == "2"):
-            self.write("hello cruel world")
+        token = self.get_argument('token', True)
+        if(token=="Qwerty12345"):
+           req = self.get_argument('req', True)
+           self.write(req)
+        else:
+            self.write("Bye")
 
-class PageTwoHandler(tornado.web.RequestHandler):
+class UserHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("take some beer")
+        method = self.get_argument('method', True)
+        if(method=="auth"):
+            login       = self.get_argument('login', True)
+            password    = self.get_argument('password', True)
+            token=api.user_auth(login,password)
+            self.write(token)
+        else:
+            token = self.get_argument('token', True)
+            if (token == "Qwerty12345"):
+                answer = api.UsersMethods(self.get_argument('method', True), self.get_argument('req', True))
+
+
+class ShopHandler(tornado.web.RequestHandler):
+    def get(self):
+        token = self.get_argument('token', True)
+    def post(self):
+        token = self.get_argument('token', True)
+
+
+class GoodHandler(tornado.web.RequestHandler):
+    def get(self):
+        token = self.get_argument('token', True)
+    def post(self):
+        token = self.get_argument('token', True)
+
+
+
+class CostHandler(tornado.web.RequestHandler):
+    def get(self):
+        token = self.get_argument('token', True)
+    def post(self):
+        token = self.get_argument('token', True)
+
+
+class BasketHandler(tornado.web.RequestHandler):
+    def get(self):
+        token = self.get_argument('token', True)
+    def post(self):
+        token = self.get_argument('token', True)
 
 
 class Application(tornado.web.Application):
     def __init__(self):
-        handlers = [('/pageone', PageOneHandler),
-                    ('/pagetwo', PageTwoHandler)]
-
+        handlers = [('/user',   UserHandler),
+                    ('/shop',   ShopHandler),
+                    ('/good',   GoodHandler),
+                    ('/cost',   CostHandler),
+                    ('/basket', BasketHandler)]
         tornado.web.Application.__init__(self, handlers)
+
 
 # Run the instance
 application = Application()
