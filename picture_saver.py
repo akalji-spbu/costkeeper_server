49ed4583ad8eff605a7bc42d4d0023a46ba0d6c8
akalji <akalji@ya.ru>
2017-04-04 11:15:46 +0300

    minor fixes, optimization

--- picture_saver.py
+++ picture_saver.py
@@ -3,14 +3,22 @@ import io
 import base64
 
 def save_picture(b64image, folder, fname):
+    status = True
+    response = ""
     byte_picture = base64.b64decode(b64image)
-    picture = Image.open(io.BytesIO(byte_picture))
-    if picture.format == "JPEG":
-        picture.close()
-        file = open(fname + ".jpg", "wb")
-        file.write(byte_picture)
-        print(picture.format)
-        print("format JPEG")
+    del b64image
+    try:
+        picture = Image.open(io.BytesIO(byte_picture))
+        if picture.format == "JPEG":
+            picture.close()
+            del picture
+            file = open(fname + ".jpg", "wb")
+            file.write(byte_picture)
+        else:
+            picture.save(fname+".jpg")
+    except OSError as e:
+        if e.args[0][0:26]=="cannot identify image file":
+            status = False
+            response = "NON_PICTURE_TYPE"
 
-    else:
-        picture.save(fname+".jpg")
+    return status, response
